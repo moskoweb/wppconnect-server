dcd35fc2edcd086aced1bdc9db9fb3dbeb964a06
Alan Mosko <moskoweb@gmail.com>
2021-08-27 12:46:37 -0300

    Update deviceController.js

--- src/controller/deviceController.js
+++ src/controller/deviceController.js
@@ -170,7 +170,7 @@ export async function getMessageById(req, res) {
 
     result = await req.client.getMessageById(messageId);
 
-    if(! result.chatId) { returnError(req, res, session, error, 'Message not found') }
+    if(result.chatId === undefined) { returnError(req, res, session, error, 'Message not found') }
 
     returnSucess(res, session, result.chatId.user, result);
   } catch (error) {
